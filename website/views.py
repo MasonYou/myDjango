14d9a6ae5e7e1b4b9f2bc48ed1af77d0da7d5d2e
Manson You <u3546642@connect.hku.hk>
2024-06-06 23:14:13 -0700

    Add delete button

--- website/views.py
+++ website/views.py
@@ -60,5 +60,12 @@ def user_record(request, pk):
         messages.success(request, "You must be logged in to view that page.")
         return redirect('home')
 
-
-
+def delete_record(request, pk):
+    if request.user.is_authenticated:
+        delete_it = Record.objects.get(id=pk)
+        delete_it.delete()
+        messages.success(request, "Record deleted.")
+        return redirect('home')
+    else:
+        messages.success(request, "You must login your account.")
+        return redirect('home')
