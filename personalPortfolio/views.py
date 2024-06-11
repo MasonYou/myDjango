70034ed20ee17eaf1f4afb8d98a697a326c02ac9
Manson You <u3546642@connect.hku.hk>
2024-06-10 22:14:49 -0700

    Refactor

--- personalPortfolio/views.py
+++ personalPortfolio/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render, redirect
 
 # Create your views here.
 def home(request):
-    return render(request, 'home/personalHome.html', {})
+    return render(request, 'home/personalWebHome.html', {})
