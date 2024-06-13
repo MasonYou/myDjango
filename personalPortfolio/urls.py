cbac7c95b92c2ad521e858af047b2f48ffd75fea
Manson You <u3546642@connect.hku.hk>
2024-06-12 22:02:55 -0700

    Add Docs folder

--- personalPortfolio/urls.py
+++ personalPortfolio/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='masonMainPage'),
+    path('Docs', views.docs, name='masonDocs')
 ]
--- personalPortfolio/views.py
+++ personalPortfolio/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render, redirect
 # Create your views here.
 def home(request):
     return render(request, 'home/personalWebHome.html', {})
+
+def docs(request):
+    return render(request, 'docs/personalWebCalendar.html', {})
