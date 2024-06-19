0799499ac19f80859a033dbaa8bb9655b1e6908f
Manson You <u3546642@connect.hku.hk>
2024-06-18 22:42:47 -0700

    Update

--- myproject/settings.py
+++ myproject/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = "django-insecure-^1x(lfspt2$!$fo%1wb%#7is0op5ao969+1a03n*dy_s^3k+pt
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+
 
 # Application definition
 
