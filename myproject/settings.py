ed65efdc4352f113198fcca63dcbfccc27e05c7a
Manson You <u3546642@connect.hku.hk>
2024-06-24 22:41:47 -0700

    Update

--- myproject/settings.py
+++ myproject/settings.py
@@ -75,18 +75,6 @@ WSGI_APPLICATION = "myproject.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': os.environ.get('DB_NAME'),
-#         'USER': os.environ.get('DB_USER'),
-#         'PASSWORD': os.environ.get('DB_PASSWORD'),
-#         'HOST': os.environ.get('DB_HOST'),
-#         'PORT': os.environ.get('DB_PORT'),
-#     }
-# }
-
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
--- personalPortfolio/views.py
+++ personalPortfolio/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 
 # Create your views here.
 def home(request):
-    return render(request, 'home/personalWebHome.html', {})
+    return render(request, 'home/ppHome.html', {})
 
 def docs(request):
-    return render(request, 'docs/personalWebDocs.html', {})
+    return render(request, 'docs/ppDocs.html', {})
