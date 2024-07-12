62eb6573bc044be92f343076fb55598049744885
Manson You <u3546642@connect.hku.hk>
2024-07-11 23:20:58 -0700

    update

--- myproject/urls.py
+++ myproject/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from world import index 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('', include("personalPortfolio.urls")),
+    path('home/', include("personalPortfolio.urls")),
     path('userControl/', include('website.urls'))
 ]
